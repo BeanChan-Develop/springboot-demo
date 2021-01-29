bcd1bd2c5e9c6b5d58d816d380c8c43df51bc398
chenbin <chenbin@dragoninfo.com.cn>
2021-01-29 13:43:09 +0800

    测试自动化构建

---
+++ src/main/java/beanchan/cn/springbootdemo/controller/HelloController.java
@@ -0,0 +1,8 @@
+package beanchan.cn.springbootdemo.controller;/**
+ * @Classname HelloController
+ * @Author ChenBin
+ * @Description 
+ * @CreateDate 2021/1/29 1:39 PM
+ */
+public class HelloController {
+}
