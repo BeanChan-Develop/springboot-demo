fdbb2892b364944afc3975800cdb9716c0261d6d
chenbin <chenbin@dragoninfo.com.cn>
2021-01-29 14:14:31 +0800

    测试自动化构建

--- src/main/java/beanchan/cn/springbootdemo/controller/HelloController.java
+++ src/main/java/beanchan/cn/springbootdemo/controller/HelloController.java
@@ -1,8 +1,18 @@
-package beanchan.cn.springbootdemo.controller;/**
+package beanchan.cn.springbootdemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
  * @Classname HelloController
  * @Author ChenBin
- * @Description 
+ * @Description
  * @CreateDate 2021/1/29 1:39 PM
  */
+@RestController
 public class HelloController {
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello World";
+    }
 }
